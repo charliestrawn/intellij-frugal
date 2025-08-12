eff5dc80e9d07113147ca0bef05d5d2e0ff3d658
Charlie Strawn <cdstrawn@gmail.com>
2025-08-11 22:12:53 -0500

    Fix compilation errors and temporarily disable LSP integration
    - Fix ASTNode null safety issue in FrugalPsiElement
    - Comment out LSP server support provider registration
    - Move LSP implementation files temporarily (LSP API not available in IntelliJ 2023.2.5)
    - This enables basic plugin functionality while LSP integration is refined
    
    Binary download warnings are expected until frugal-ls releases are available.

--- src/main/kotlin/dev/strawn/frugal/FrugalPsiElement.kt
+++ src/main/kotlin/dev/strawn/frugal/FrugalPsiElement.kt
@@ -3,4 +3,4 @@ package dev.strawn.frugal
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 
-class FrugalPsiElement(node: ASTNode?) : ASTWrapperPsiElement(node)
+class FrugalPsiElement(node: ASTNode?) : ASTWrapperPsiElement(node!!)
